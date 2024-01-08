b4b4d59f21ee60193d08fa7bbaf03a59657817f9
Ricardo Villalobos Vargas <ricardo_villalobosv@hotmail.com>
2024-01-08 15:04:39 -0500

    CHG: media

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    // animate(
-    //   '.hero__title',
-    //   { opacity: 1, x: 0 },
-    //   { delay: 0.5, duration: 0.9, easing: 'ease-in-out' }
-    // )
+export class AppComponent implements AfterViewInit {
+  @ViewChild('music') playerRef: ElementRef<HTMLAudioElement>;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.playerRef.nativeElement.play();
+    this.playerRef.nativeElement.volume = 0.25;
   }
 }
